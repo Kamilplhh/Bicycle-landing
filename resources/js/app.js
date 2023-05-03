171cb0cb5a6e9ec16ed918eef38fb3a6058d4a14
Kamilplhh <Kamil_PLHH@wp.pl>
2023-05-03 20:30:14 +0200

    Title animation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,70 @@
 import './bootstrap';
+import $ from 'jquery';
+window.$ = $;
+
+let position = $(window).scrollTop();
+
+$(window).scroll(function () {
+    setTimeout(() => {
+        let bike = "🚲";
+        let dash = "_";
+        let title = $(document).attr('title');
+        let NewTitle = "";
+
+        let scroll = $(window).scrollTop();
+        if (scroll > position) {
+            let x = title.indexOf(bike);
+            x = x - 1;
+
+            if (x == 0) {
+                NewTitle += bike;
+                for (let i = 0; i < 27; i++) {
+                    NewTitle += dash;
+                }
+            } else if (x < 0) {
+                for (let i = 0; i < 27; i++) {
+                    NewTitle += dash;
+                }
+                NewTitle += bike;
+            } else if (x > 0) {
+                for (let i = 0; i < x; i++) {
+                    NewTitle += dash;
+                }
+                NewTitle += bike;
+                let z = 27 - x;
+                if (z > 0) {
+                    for (let i = 0; i <= z; i++) {
+                        NewTitle += dash;
+                    }
+                }
+            }
+            $(document).attr("title", NewTitle);
+        }
+
+        else if (scroll < position){
+            let x = title.indexOf(bike);
+            x = x + 1;
+
+            if (x == 27 || x > 27) {
+                NewTitle += bike;
+                for (let i = 0; i < 27; i++) {
+                    NewTitle += dash;
+                }
+            } else if (x < 27 && x > 0) {
+                for (let i = 0; i < x; i++) {
+                    NewTitle += dash;
+                }
+                NewTitle += bike;
+                let z = 27 - x;
+                if (z > 0) {
+                    for (let i = 0; i <= z; i++) {
+                        NewTitle += dash;
+                    }
+                }
+            }
+            $(document).attr("title", NewTitle);
+        }
+        position = scroll;
+    }, "500");
+    
+});
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title> __________________________🚲</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @vite('resources/js/app.js')
+    @section('navbar')
+
+    @show
+        @yield('content')
+        
+    </div>     
+</body>
+
+</html>
